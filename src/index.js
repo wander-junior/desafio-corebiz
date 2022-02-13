c66ad2e79afc37753ef1ebcbcd5b31fed7604e28
Wander Júnior Cruz <wander.cruz@enext.com.br>
2022-02-13 18:19:07 -0300

    Configure babel and webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+const App = () => {
+    return <h1>Desafio CoreBiz</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
