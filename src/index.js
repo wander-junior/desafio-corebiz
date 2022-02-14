37d72d558d27a4bd79df06c8196e89c7dfcb9a96
wander-junior <wanderjrcruz96@gmail.com>
2022-02-14 03:07:05 -0300

    Install React Router and create home page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import GlobalStyle from "./globalStyles";
 import Theme from "./Theme";
 
-import Header from "./components/Header";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
-        <Theme>
-            <GlobalStyle />
-            <Header />
-        </Theme>
+        <BrowserRouter>
+            <Theme>
+                <GlobalStyle />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Theme>
+        </BrowserRouter>
     );
 }
 
