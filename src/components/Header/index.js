4afc8cb2e0c526903b739eceee3b742a968b0136
wander-junior <wanderjrcruz96@gmail.com>
2022-02-14 02:54:10 -0300

    Stylize Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-import { HeaderWrapper } from './styles';
+import { HeaderWrapper, LogoHeader, SearchForm, SearchBar, SearchButton, UserAccount, MiniCart, ItemQuantity } from './styles';
 
 import logo from '../../assets/img/logo.svg'
 import user from '../../assets/img/user.svg'
 import shoppingCart from '../../assets/img/shoppingCart.svg'
+import searchIcon from '../../assets/img/searchIcon.svg'
 
 export default function Header() {
-  return (
-    <HeaderWrapper>
-        <img src={logo} alt="Logo coreBiz" />
-        <input type={"text"} />
-        <div>
-            <img src={user} alt="Ícone de usuário" />
-            Minha conta
-        </div>
-        <div>
-            <img src={shoppingCart} alt="user" />
-            0
-        </div>
-    </HeaderWrapper>
-  )
+    const handleSubmit = ((event) => event.preventDefault());
+
+    return (
+        <HeaderWrapper>
+            <LogoHeader src={logo} alt="Logo coreBiz" />
+            <SearchForm onSubmit={handleSubmit}>
+                <SearchBar placeholder="O que está procurando?" type={"text"} />
+                <SearchButton type="submit">
+                    <img src={searchIcon} alt="Ícone de lupa para pesquisa" />
+                </SearchButton>
+            </SearchForm>
+            <UserAccount>
+                <img src={user} alt="Ícone de usuário"  />
+                Minha conta
+            </UserAccount>
+            <MiniCart>
+                <img src={shoppingCart} alt="user" />
+                <ItemQuantity>0</ItemQuantity>
+            </MiniCart>
+        </HeaderWrapper>
+    )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,79 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
+    width: 1238px;
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
+`;
+
+export const LogoHeader = styled.img`
+    margin: 16px 132px 24px 0;
+`;
+
+export const SearchForm = styled.form`
+    display: flex;
+    flex-direction: row;
+    width: 718px;
+    margin-right: 44px;
+`;
+
+export const SearchBar = styled.input`
+    outline: none;
+    border-width: 0 0 1px 0;
+    border-color: #7A7A7A;
+    width: 100%;
+    height: 28px;
+    font-family: '${({theme}) => theme.fonts[0]}';
+    font-size: 0.8125rem;
+
+    ::placeholder {
+        color: #7A7A7A;
+        opacity: 1;
+    }
+    
+    :-ms-input-placeholder {
+        color: #7A7A7A;
+    }
+    
+    ::-ms-input-placeholder {
+        color: #7A7A7A;
+    }
+`;
+
+export const SearchButton = styled.button`
+    background: transparent;
+    border: none;
+    outline: none;
+    margin-left: -18px;
+    cursor: pointer;
+`;
+
+export const UserAccount = styled.div`
+    font-size: 0.8125rem;
+    color: #7A7A7A;
+    display: flex;
+    margin-right: 21px;
+
+    img {
+        margin-right: 5px;
+    }
+`;
+
+export const MiniCart = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const ItemQuantity = styled.div`
+    font-size: 0.625rem;
+    font-weight: bold;
+    color: #ffffff;
+    width: 14px;
+    height: 14px;
+    border-radius: 50%;
+    background: #F8475F;
+    text-align: center;
+    margin-left: 5px;
 
 `;
