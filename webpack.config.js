134c803215e3a98c4ae5b086c246552456b8d892
wander-junior <wanderjrcruz96@gmail.com>
2022-02-14 00:30:05 -0300

    Configure svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,19 @@ module.exports = {
         {
             test: /\.css$/,
             use: ["style-loader", "css-loader"]
-        }
+        },
+        {
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: 'svg-url-loader',
+                    options: {
+                        limit: 10000,
+                    },
+                },
+            ],
+        },
+    
     ]},
     plugins: [htmlPlugin]
 };
