b014eb362411d04a127f01c96b73d2b64fa0f753
wander-junior <wanderjrcruz96@gmail.com>
2022-02-16 03:35:37 -0300

    Create News component

---
+++ src/components/News/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function News() {
+  return (
+    <section>
+        <h3>Participe de nossas news com promoções e novidades!</h3>
+        <form>
+            <input type="text" />
+            <input type="email" />
+            <button>Eu quero</button>
+        </form>
+    </section>
+  )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import Header from "../../components/Header";
-import Shelf from '../../components/shelf';
+import Shelf from '../../components/Shelf';
+import News from '../../components/News';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Shelf />
+      <News />
     </>
   )
 }
